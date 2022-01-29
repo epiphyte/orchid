b172e94118add25766d9cc71f5a32b4ec85fac59
Fernandez-Alcon, Jose <jose@epiphyte.io>
2022-01-29 13:19:59 -0500

    Adds comments to the functions. refs #6

--- orchid.go
+++ orchid.go
@@ -24,7 +24,7 @@ const (
 	COLOR_DEBUG = "\033[48;5;5m"
 )
 
-//Describes the structure of a log message
+//logMessage struct describes a log message
 type logMessage struct {
 	Severity string    //The severity of the message [INFO, DEBUG, SUCCESS, WARNING, ERROR, FATAL]
 	Text     string    //The contents of the log
@@ -32,16 +32,19 @@ type logMessage struct {
 	Time     time.Time // The time at which the log was created
 }
 
+//Init initializes de module. It sets a name of the module calling the logger to filter the logs
 func Init(module_name string) {
 	module = module_name
 }
 
+//createLogMessage is internal helper to fill in the logMessage struct
 func (l *logMessage) createLogMessage(severity string, a ...interface{}) {
 	l.Time = time.Now()
 	l.Text = fmt.Sprint(a...)
 	l.Severity = severity
 }
 
+//printLogMessage is the internal function that colorizes and pretty prints the log message
 func (l *logMessage) printLogMessage() {
 	metadata := fmt.Sprintf("%-20s %-6s", module, l.Severity)
 	color := COLOR_INFO
@@ -72,36 +75,43 @@ func (l *logMessage) printLogMessage() {
 	}
 }
 
+//Info prints a log message of the severity level Info which has blue background on the terminal
 func Info(a ...interface{}) {
 	var l logMessage
 	l.createLogMessage("INFO", a...)
 	l.printLogMessage()
 }
 
+//OK prints a log message of the severity level OK which has green background on the terminal
 func OK(a ...interface{}) {
 	var l logMessage
 	l.createLogMessage("OK", a...)
 	l.printLogMessage()
 }
 
+//Error prints a log message of the severity level Error which has red background on the terminal
 func Error(a ...interface{}) {
 	var l logMessage
 	l.createLogMessage("ERROR", a...)
 	l.printLogMessage()
 }
 
+//Fatal prints a log message of the severity level Fatal which has red background on the terminal and terminates the
+//execution of the program
 func Fatal(a ...interface{}) {
 	var l logMessage
 	l.createLogMessage("FATAL", a...)
 	l.printLogMessage()
 }
 
+//Warn prints a log message of the severity level Warn which has yellow background on the terminal
 func Warn(a ...interface{}) {
 	var l logMessage
 	l.createLogMessage("WARN", a...)
 	l.printLogMessage()
 }
 
+//Debug prints a log message of the severity level Debug which has purple background on the terminal
 func Debug(a ...interface{}) {
 	var l logMessage
 	l.createLogMessage("DEBUG", a...)
