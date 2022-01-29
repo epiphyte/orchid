9dbddc3b331450f5e007fa37614360b306c3cc9a
Fernandez-Alcon, Jose <jose@epiphyte.io>
2022-01-29 11:31:28 -0500

    [TASK] Adds copyright header to the source files. refs #5.

--- orchid_test.go
+++ orchid_test.go
@@ -1,3 +1,9 @@
+// Package orchid
+//Copyright (c) 2022 Epiphyte LLC. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+// Author: Fernandez-Alcon, Jose
+// e-mail: jose@epiphyte.io
 package orchid
 
 import "testing"
